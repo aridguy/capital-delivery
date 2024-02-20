9efe2b602e3c917883bfc3d8c4e82f490c720491
aridguy <rotimiariyo@gmail.com>
2024-02-20 02:50:14 +0100

    added all files

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@
 // preloader
 function loader() {
 	$('#ctn-preloader').addClass('loaded');
-	$("#loading").fadeOut(500);
+	$("#loading").fadeOut(200);
 	// Una vez haya terminado el preloader aparezca el scroll
 
 	if ($('#ctn-preloader').hasClass('loaded')) {
 		// Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
-		$('#preloader').delay(900).queue(function () {
+		$('#preloader').delay(500).queue(function () {
 			$(this).remove();
 		});
 	}
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.use(express.static('public')); // Serve static files from the 'public' folder
+// app.use(express.static('public')); // Serve static files from the 'public' folder
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
